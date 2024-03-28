805260f999c36a2855e31a9b5e2ae8f8ba78c250
michaelgeiger <michaelgeiger@hey.com>
2024-03-28 11:55:40 -0400

    generating Endpoint library for JSONAPI:SPEC
    npx nx g @nx/js:library endpoint-jsonapi --d
    irectory=libs/endpoint/jsonapi --allPrompts
    
    With the following args
    ✔ Which unit test runner would you like to use? · vitest
    ✔ Which bundler would you like to use to build the library? Choose 'none' to skip build setup. · vite
    ✔ What should be the project name and where should it be generated? · endpoint-jsonapi @ libs/endpoint/jsonapi

---
+++ libs/endpoint/jsonapi/src/lib/endpoint-jsonapi.spec.ts
@@ -0,0 +1,7 @@
+import { endpointJsonapi } from './endpoint-jsonapi';
+
+describe('endpointJsonapi', () => {
+  it('should work', () => {
+    expect(endpointJsonapi()).toEqual('endpoint-jsonapi');
+  });
+});
